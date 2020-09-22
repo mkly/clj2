59f249d8e17d07bb1c52bcc037f76308d477d804
Mike Lay <mike@mkly.io>
2020-09-22 08:59:39 -0700

    add another route

--- src/work/handler.clj
+++ src/work/handler.clj
@@ -10,3 +10,11 @@
                       [:title (str "Hello " name)]
                       [:body
                        [:h1 {:style "text-align: center"} (str "Hello " name)]]])]))
+
+(defmethod ig/init-key :work.handler/another [_ _options]
+  (fn [{[_ name] :ataraxy/result}]
+    [::response/ok (html
+                     [:head
+                      [:title (str "Another" name)]
+                      [:body
+                       [:h1 {:style "text-align: center"} (str "Another")]]])]))
