5c49b1a88c93ef9433e0c24267bb024ce1f1583b
Mike Lay <mike@mkly.io>
2020-09-22 00:01:56 -0700

    Initial commit

---
+++ src/work/handler.clj
@@ -0,0 +1,12 @@
+(ns work.handler
+  (:require [ataraxy.response :as response] 
+            [hiccup.core :refer [html]]
+            [integrant.core :as ig]))
+
+(defmethod ig/init-key :work.handler/hello [_ _options]
+  (fn [{[_ name] :ataraxy/result}]
+    [::response/ok (html
+                     [:head
+                      [:title (str "Hello " name)]
+                      [:body
+                       [:h1 {:style "text-align: center"} (str "Hello " name)]]])]))
